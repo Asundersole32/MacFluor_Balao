c7b151dbf8103aac560cfa1f4430a2afaa0f015e
Erik Felipe(PC casa) <erikviana32@gmail.com>
2024-04-28 16:33:10 -0400

    Add the change the balloon diameter

--- main.py
+++ main.py
@@ -44,16 +44,29 @@ def main():
         print("Type a acceptable scale!")
         return main()
 
-    choice = input("Do you want to add persons on the balloon?(yes or no): ")
-    choice = choice.lower()
+    choice1 = input("Do you want to add persons on the balloon?(yes or no): ")
+    choice1 = choice1.lower()
 
-    if choice == "yes":
+    if choice1 == "yes":
         persons_weight = float(input("Insert the persons weight(in kilograms): "))
         persons_newton = kilogram_to_newton(persons_weight)
         new_load = add_persons(persons_newton, load)
         air_temp = activity_2(diameter, new_load, converted_temp, gravity, atmosphere_pressure)
         return air_temp
-    elif choice == "no":
+    elif choice1 == "no":
+        air_temp = activity_2(diameter, load, converted_temp, gravity, atmosphere_pressure)
+        return air_temp
+    else:
+        print("Please, answer with yes or no!")
+        return main()
+
+    choice2 = input("Do you want to change the balloon diameter?(yes or no): ")
+    choice2 = choice2.lower()
+
+    if choice2 == "yes":
+        new_diameter = float(input("Please, insert the new diameter: "))
+        air_temp = activity_2(new_diameter, new_load, converted_temp, gravity, atmosphere_pressure)
+    elif choice2 == "no":
         air_temp = activity_2(diameter, load, converted_temp, gravity, atmosphere_pressure)
         return air_temp
     else:
